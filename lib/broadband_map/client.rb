a9b4ecce73e436f5050b1eb88e765b88a71c8e73
Ryan Resella <ryan@codeforamerica.org>
2011-06-14 14:31:40 -0700

    Updated client for connection and request

--- lib/broadband_map/client.rb
+++ lib/broadband_map/client.rb
@@ -1,5 +1,12 @@
+require 'broadband_map/client'
+require 'broadband_map/client/connection'
+require 'broadband_map/client/almanac'
+
 module BroadbandMap
   class Client
-
+    include BroadbandMap::Client::Connection
+    include BroadbandMap::Client::Request
+    
+    include BroadbandMap::Client::Almanac
   end
 end
