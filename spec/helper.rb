aa2970a7b65c7b68df366f2ed744ab76bfee59bf
Ryan Resella <ryan@codeforamerica.org>
2011-06-14 14:10:32 -0700

    Added web address to helper spec

--- spec/helper.rb
+++ spec/helper.rb
@@ -4,3 +4,45 @@ require 'simplecov'
 SimpleCov.start
 require 'broadband_map'
 require 'rspec'
+
+require 'webmock/rspec'
+
+def a_delete(path)
+  a_request(:delete, 'http://www.broadbandmap.gov/broadbandmap/' + path)
+end
+
+def a_get(path)
+  a_request(:get, 'http://www.broadbandmap.gov/broadbandmap/' + path)
+end
+
+def a_post(path)
+  a_request(:post, 'http://www.broadbandmap.gov/broadbandmap/' + path)
+end
+
+def a_put(path)
+  a_request(:put, 'http://www.broadbandmap.gov/broadbandmap/' + path)
+end
+
+def stub_delete(path)
+  stub_request(:delete, 'http://www.broadbandmap.gov/broadbandmap/' + path)
+end
+
+def stub_get(path)
+  stub_request(:get, 'http://www.broadbandmap.gov/broadbandmap/' + path)
+end
+
+def stub_post(path)
+  stub_request(:post, 'http://www.broadbandmap.gov/broadbandmap/' + path)
+end
+
+def stub_put(path)
+  stub_request(:put, 'http://www.broadbandmap.gov/broadbandmap/' + path)
+end
+
+def fixture_path
+  File.expand_path('../fixtures', __FILE__)
+end
+
+def fixture(file)
+  File.new(fixture_path + '/' + file)
+end
