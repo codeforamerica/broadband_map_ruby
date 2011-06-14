2acc3cf0bc4c3088738bf582eaa49b2002f4a9ee
Ryan Resella <ryan@codeforamerica.org>
2011-06-14 14:50:00 -0700

    Added documentation for almanac_parameters

--- lib/broadband_map/client/almanac.rb
+++ lib/broadband_map/client/almanac.rb
@@ -1,9 +1,18 @@
 module BroadbandMap
  class Client
    module Almanac
+     
+     # Returns all almanac parameters used by the Almanac API for ranking.
+     #
+     # @param options [Hash] A customizable set of options. 
+     # @return {Hash}
+     # @see http://www.broadbandmap.gov/developer/api/almanac-api-parameters
+     # @example
+     #   almanac_parameters()
      def almanac_parameters(options={})
        get("almanac/parameters?format=json", options)
      end
+     
    end
  end 
 end
