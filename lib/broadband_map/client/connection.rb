09af3085b1f204c535eabdbf5667fa13654a7f7c
Ryan Resella <ryan@codeforamerica.org>
2011-06-14 14:27:06 -0700

    Added connection

---
+++ lib/broadband_map/client/connection.rb
@@ -0,0 +1,19 @@
+require 'faraday_middleware'
+
+module BroadbandMap
+  class Client
+    module Connection
+      private
+
+      def connection
+        Faraday.new(:url => 'http://www.broadbandmap.gov/broadbandmap/') do |connection|
+          connection.use Faraday::Request::UrlEncoded
+          connection.use Faraday::Response::RaiseError
+          connection.use Faraday::Response::Rashify
+          connection.use Faraday::Response::ParseJson
+          connection.adapter(Faraday.default_adapter)
+        end
+      end
+    end
+  end
+end
