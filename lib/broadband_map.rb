1b3260d8d9984adab8bc7a175579811e8c047e27
Ryan Resella <ryan@codeforamerica.org>
2011-06-14 13:57:12 -0700

    Added method missing in initialization

--- lib/broadband_map.rb
+++ lib/broadband_map.rb
@@ -1,3 +1,17 @@
 module BroadbandMap
-  # Your code goes here...
+  
+  def self.client(options={})
+      BroadbandMap::Client.new(options)
+    end
+
+    # Delegate to BroadbandMap::Client.new
+    def self.method_missing(method, *args, &block)
+      return super unless client.respond_to?(method)
+      client.send(method, *args, &block)
+    end
+
+    def self.respond_to?(method, include_private=false)
+      client.respond_to?(method, include_private) || super(method, include_private)
+    end
+    
 end
