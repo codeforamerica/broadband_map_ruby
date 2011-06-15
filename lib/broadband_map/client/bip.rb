d7f547189ecf5d06ac2c229fd5a72226d39528f4
blakeshall <blake@blakeshall.com>
2011-06-15 12:23:14 -0700

    Added documentation for bip_state_id

--- lib/broadband_map/client/bip.rb
+++ lib/broadband_map/client/bip.rb
@@ -2,6 +2,13 @@ module BroadbandMap
   class Client
     module Bip
       
+      # Returns the BIP funding allocated to states by specifying the state fips code
+      # @param params [Hash] The parameters for the lookup
+      # @param options [Hash] A customizable set of options. 
+      # @return {Hash}
+      # @see http://www.broadbandmap.gov/developer/api/bip-funding-api-by-state-id
+      # @example
+      #   bip_state_id({:state_ids => ["01", "02"]})
       def bip_state_id(params={}, options={})
         params = {:format => 'json'}.merge(params)
         ids = ""
