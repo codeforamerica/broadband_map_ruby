30fe694c1d02ce137debb0c014c93bc1d0dd1977
blakeshall <blake@blakeshall.com>
2011-06-15 14:13:12 -0700

    Added provider_name spec

--- spec/broadband_map/providers_spec.rb
+++ spec/broadband_map/providers_spec.rb
@@ -18,5 +18,17 @@ describe BroadbandMap::Client::Bip do
       test.results[0].holdingCompanyNumber.should == "131413"
     end
   end
-
+  
+  describe ".provider_name" do
+    before do
+      stub_get ("provider/name/alb?format=json&callback=").
+        to_return(:status => 200, :body => fixture("providers_name.json"))
+    end
+    
+    it "should return correct item" do
+      test = @client.provider({:name => 'alb'})
+      a_get("provider/name/alb?format=json&callback=")
+      test.results[0].holdingCompanyNumber.should == '130031'
+    end
+  end
 end
