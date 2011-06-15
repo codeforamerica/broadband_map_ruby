6c87c7e31c47f54a93ec5992de95414f9a09c3f5
blakeshall <blake@blakeshall.com>
2011-06-15 16:44:41 -0700

    Added cai_closest spec

---
+++ spec/broadband_map/cai_spec.rb
@@ -0,0 +1,22 @@
+require 'helper'
+
+describe BroadbandMap::Client::Cai do
+
+  before do
+    @client = BroadbandMap::Client.new
+  end
+  
+  describe ".cai_closest" do
+    before do
+      sub_get("cai/closest?latitude=41.486857&longitude=-71.294392&maxresults=2&format=json&callback=").
+        to_return(:status => 200, :body => fixture("cai_closest.json"))
+    end
+    
+    it "should return the correct item" do
+      test = @client.cai_closest({:latitude => 41.486857, :longitude => -71.294392, :max_results=> 2})
+      a_call("cai/closest?latitude=41.486857&longitude=-71.294392&maxresults=2&format=json&callback=").should have_been_made
+      test.results[0].latitude.should == 41.49
+    end
+  end
+
+end
