cb3cea7170e4aaf4fbaef280cd5e52be080bd329
blakeshall <blake@blakeshall.com>
2011-06-15 15:04:41 -0700

    Added documentation for summary_geo

--- lib/broadband_map/client/summary.rb
+++ lib/broadband_map/client/summary.rb
@@ -2,6 +2,15 @@ module BroadbandMap
   class Client
     module Summary
 
+      # Returns broadband summary data by geography IDs for a specific geography type
+      #
+      # @param params [Hash] :data_version, :census_metric_type, :geography_type (optional :format, :callback)
+      # @param options [Hash] A customizable set of options. 
+      # @return {Hash}
+      # @see http://www.broadbandmap.gov/developer/api/broadband-summary-api-by-geography-type-and-geography-id
+      # @example
+      #   summary_geo({:data_version => 'fall2010', :census_metric_type => 'population', :geography_type => 'state', :geography_ids => ['10']})
+
       def summary_geo(params={}, options={})
         params = {:format => 'json'}.merge(params)
         ids = ""
