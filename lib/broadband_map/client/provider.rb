da47ab27e05f6e19c64a3738d9508502d0350d7c
blakeshall <blake@blakeshall.com>
2011-06-15 14:06:51 -0700

    Added provider_all doc

--- lib/broadband_map/client/provider.rb
+++ lib/broadband_map/client/provider.rb
@@ -2,6 +2,15 @@ module BroadbandMap
   class Client
     module Provider
       
+      # Returns all providers.
+      #
+      # @param params [Hash] options for the lookup.
+      # @param options [Hash] A customizable set of options. 
+      # @return {Hash}
+      # @see http://www.broadbandmap.gov/developer/api/broadband-provider-api-all-providers
+      # @example
+      #   provider_all()
+      
       def provider_all(params={}, options={})
         params = {:format => 'json'}.merge(params)
         get("provider?format=#{params[:format]}&callback=#{params[:callback]}")
