ebef2f9f92baa1e15fdd7e15cb1edccad72fa494
blakeshall <blake@blakeshall.com>
2011-06-15 12:37:59 -0700

    Added bip_state_name method and documentation

--- lib/broadband_map/client/bip.rb
+++ lib/broadband_map/client/bip.rb
@@ -17,6 +17,22 @@ module BroadbandMap
         get("bip/stateids/#{ids}?format=#{params[:format]}&callback=#{params[:callback]}", options)
       end
       
+      # Returns the BIP funding allocated to states by specifying the state name
+      # @param params [Hash] The parameters for the lookup
+      # @param options [Hash] A customizable set of options. 
+      # @return {Hash}
+      # @see http://www.broadbandmap.gov/developer/api/bip-funding-api-by-state-name
+      # @example
+      #   bip_state_id({:state_names => ['alaska', 'alabama']})
+      
+      def bip_state_name(params={}, options={})
+        params = {:format => 'json'}.merge(params)
+        names = ""
+        params[:state_names].each {|x| names += x +","}
+        names.chop!
+        get("bip/states/#{names}?format=#{params[:format]}&callback=#{params[:callback]}", options)
+      end
+      
     end
   end
 end
