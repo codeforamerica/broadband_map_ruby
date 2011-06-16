266891bf284c92db3b0cdefcfd7847f9d4b1287c
blakeshall <blake@blakeshall.com>
2011-06-15 17:49:44 -0700

    Added demographics_geo_id method and fixed it's test

--- lib/broadband_map/client/demographics.rb
+++ lib/broadband_map/client/demographics.rb
@@ -7,6 +7,13 @@ module BroadbandMap
         get("demographic/#{params[:data_version]}/coordinates?latitude=#{params[:latitude]}&longitude=#{params[:longitude]}&format=#{params[:format]}&callback=#{params[:callback]}")
       end
    
+      def demographics_geo_id(params={}, options={})
+        params = {:format => 'json'}.merge(params)
+        ids = ""
+        params[:geography_ids].each {|x| ids += x +","}
+        ids.chop!
+        get("demographic/#{params[:data_version]}/#{params[:geography_type]}/ids/#{ids}?format=#{params[:format]}&callback=#{params[:callback]}")
+      end
       
     end
   end
--- spec/broadband_map/demographics_spec.rb
+++ spec/broadband_map/demographics_spec.rb
@@ -27,7 +27,7 @@ describe BroadbandMap::Client::Demographics do
     
     it "should return the correct item" do
       test = @client.demographics_geo_id({:data_version => 'fall2010', :geography_type => 'county', :geography_ids => ['17081', '17083']})
-      a_call("demographic/fall2010/county/ids/17081,17083?format=json&callback=").should have_been_made
+      a_get("demographic/fall2010/county/ids/17081,17083?format=json&callback=").should have_been_made
       test.results[0].geographyId.should == '17083'
     end
   end
