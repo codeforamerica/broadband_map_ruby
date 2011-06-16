222c827b0d856c638de260853888b60b70ce79d3
blakeshall <blake@blakeshall.com>
2011-06-15 17:46:20 -0700

    Added demographics_geo_id spec

--- spec/broadband_map/demographics_spec.rb
+++ spec/broadband_map/demographics_spec.rb
@@ -19,4 +19,17 @@ describe BroadbandMap::Client::Demographics do
     end
   end
   
+  describe '.demographics_geo_id' do
+    before do
+      stub_get("demographic/fall2010/county/ids/17081,17083?format=json&callback=").
+        to_return(:status => 200, :body => fixture("demo_geo_id.json"))
+    end
+    
+    it "should return the correct item" do
+      test = @client.demographics_geo_id({:data_version => 'fall2010', :geography_type => 'county', :geography_ids => ['17081', '17083']})
+      a_call("demographic/fall2010/county/ids/17081,17083?format=json&callback=").should have_been_made
+      test.results[0].geographyId.should == '17083'
+    end
+  end
+  
 end
