95709b259829123c349143ccf333fe2bb299327c
blakeshall <blake@blakeshall.com>
2011-06-16 14:58:01 -0700

    Added speed_test documentation

--- lib/broadband_map/client/speed_test.rb
+++ lib/broadband_map/client/speed_test.rb
@@ -2,6 +2,14 @@ module BroadbandMap
   class Client
     module SpeedTest
       
+      # Returns the speed test results for a particular geography type (e.g., state, congressional district) and geography ID.
+      # @param params [Hash] :geogrpahy_type and :geography_ids and optional :speed_test_type, :format, :callback
+      # @param options [Hash] A customizable set of options. 
+      # @return {Hash}
+      # @see http://www.broadbandmap.gov/developer/api/speed-test-api-by-geography-type-and-geography-id
+      # @example
+      #   speed_test_geo_type_id({:geography_type => 'state', :geography_ids => ['01', '02']})
+      
       def speed_test_geo_type_id(params={}, options={})
         params = {:format => 'json'}.merge(params)
         ids = ""
@@ -9,6 +17,14 @@ module BroadbandMap
         ids.chop!
         get("speedtest/#{params[:geography_type]}/ids/#{ids}?format=#{params[:format]}&speedtesttype=#{params[:speed_test_type]}&callback=#{params[:callback]}")
       end
+      
+      # Returns the speed test results for a particular geography type (e.g., state, congressional district) and geography name (e.g., Virginia).
+      # @param params [Hash] :geogrpahy_type and :geography_names and optional :speed_test_type, :format, :callback
+      # @param options [Hash] A customizable set of options. 
+      # @return {Hash}
+      # @see http://www.broadbandmap.gov/developer/api/speed-test-api-by-geography-type-and-geography-name
+      # @example
+      #   speed_test_geo_type_name({:geography_type => 'state', :geography_names => ['alabama', 'arizona']})
       
       def speed_test_geo_type_name(params={}, options={})
         params = {:format => 'json'}.merge(params)
@@ -18,10 +34,26 @@ module BroadbandMap
         get("speedtest/#{params[:geography_type]}/names/#{names}?format=#{params[:format]}&speedtesttype=#{params[:speed_test_type]}&callback=#{params[:callback]}")
       end
       
+      # Returns the minimum and maximum quartile speeds by geography type within the nation.
+      # @param params [Hash] :geogrpahy_type and optional :speed_test_type, :format, :callback
+      # @param options [Hash] A customizable set of options. 
+      # @return {Hash}
+      # @see http://www.broadbandmap.gov/developer/api/speed-test-api-minimum-and-maximum-quartile-speeds-by-geography-type
+      # @example
+      #   speed_test_quartile({:geography_type => 'state'})
+      
       def speed_test_quartile(params={}, options={})
         params = {:format => 'json'}.merge(params)
         get("speedtest/#{params[:geography_type]}/quartile?format=#{params[:format]}&speedtesttype=#{params[:speed_test_type]}&callback=#{params[:callback]}")
       end   
+      
+      # Returns all the speed test results for the entire United States.
+      # @param params [Hash] optional :speed_test_type, :format, :callback
+      # @param options [Hash] A customizable set of options. 
+      # @return {Hash}
+      # @see http://www.broadbandmap.gov/developer/api/speed-test-api-nation
+      # @example
+      #   speed_test_nation()
       
       def speed_test_nation(params={}, options={})
         params = {:format => 'json'}.merge(params)
