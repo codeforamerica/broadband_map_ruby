448d6ec775e21f106cb25501b2e931ac892a5dce
blakeshall <blake@blakeshall.com>
2011-06-16 11:24:26 -0700

    added speed_test_geo_type_id spec

---
+++ spec/broadband_map/speed_test_spec.rb
@@ -0,0 +1,22 @@
+require 'helper'
+
+describe BroadbandMap::Client::SpeedTest do
+
+  before do
+    @client = BroadbandMap::Client.new
+  end
+  
+  describe ".speed_test_geo_type_id" do
+    before do
+      stub_get("speedtest/state/ids/01,02?format=json&speedtesttype=callback=").
+        to_return(:status => 200, :body => fixture("speed_geo_type_id.json"))
+    end
+    
+    it "should get the correct item" do
+      test = @client.speed_test_geo_type_id({:geography_type => 'state', :geography_ids => ['01', '02']})
+      a_get("speedtest/state/ids/01,02?format=json&speedtesttype=callback=").should have_been_made
+      test.results[0].geographyId.should == "01"
+    end
+  end
+  
+end
