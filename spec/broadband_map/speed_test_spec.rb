ddff0d2df861faa1f338d6c473373cd03ee1c463
blakeshall <blake@blakeshall.com>
2011-06-16 11:34:33 -0700

    Added speed_test_quartile spec and fixed other specs without & between speedtesttype and callback

--- spec/broadband_map/speed_test_spec.rb
+++ spec/broadband_map/speed_test_spec.rb
@@ -8,27 +8,40 @@ describe BroadbandMap::Client::SpeedTest do
   
   describe ".speed_test_geo_type_id" do
     before do
-      stub_get("speedtest/state/ids/01,02?format=json&speedtesttype=callback=").
+      stub_get("speedtest/state/ids/01,02?format=json&speedtesttype=&callback=").
         to_return(:status => 200, :body => fixture("speed_geo_type_id.json"))
     end
     
     it "should get the correct item" do
       test = @client.speed_test_geo_type_id({:geography_type => 'state', :geography_ids => ['01', '02']})
-      a_get("speedtest/state/ids/01,02?format=json&speedtesttype=callback=").should have_been_made
+      a_get("speedtest/state/ids/01,02?format=json&speedtesttype=&callback=").should have_been_made
       test.results[0].geographyId.should == "01"
     end
   end
   
   describe ".speed_test_geo_type_name" do
     before do
-      stub_get("speedtest/state/names/alabama,arizona?format=json&speedtesttype=callback=").
+      stub_get("speedtest/state/names/alabama,arizona?format=json&speedtesttype=&callback=").
         to_return(:status => 200, :body => fixture("speed_geo_type_id.json"))
     end
     
     it "should get the correct item" do
       test = @client.speed_test_geo_type_name({:geography_type => 'state', :geography_names => ['alabama', 'arizona']})
-      a_get("speedtest/state/names/alabama,arizona?format=json&speedtesttype=callback=").should have_been_made
+      a_get("speedtest/state/names/alabama,arizona?format=json&speedtesttype=&callback=").should have_been_made
       test.results[0].geographyId.should == "01"
+    end
+  end
+  
+  describe ".speed_test_quartile" do
+    before do
+      stub_get("speedtest/state/quartile?format=json&speedtesttype=&callback=").
+        to_return(:status => 200, :body => fixture("speed_quartile_type.json"))
+    end
+    
+    it "should get the correct item" do
+      test = @client.speed_test_quartile({:geography_type => 'state'})
+      a_get("speedtest/state/quartile?format=json&speedtesttype=&callback=").should have_been_made
+      test.results.minimumLowerQuartileDownload.should == 0.00781
     end
   end
   
